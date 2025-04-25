1dc1cbaa3907796ab5d6266770eda92138e7cceb
Kind606 <kevin.hellgren@hotmail.com>
2025-04-25 11:30:03 +0200

    Fixed mongoDB models

---
+++ server/src/posts/post-model.ts
@@ -0,0 +1,11 @@
+import { InferSchemaType, model, Schema, Types } from "mongoose";
+
+const postSchema = new Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: Types.ObjectId, required: true },
+  });
+
+export const PostModel = model("Post", postSchema);
+
+export type Post = InferSchemaType<typeof postSchema>;
--- server/src/users/user-model.ts
+++ server/src/users/user-model.ts
@@ -1,7 +1,7 @@
 import { InferSchemaType, model, Schema } from "mongoose";
 
 const userSchema = new Schema({
-    user: { type: String, required: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now() },
