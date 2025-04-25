d9213809ef9c1d54589470837f08bd4ac8328a57
Tomas Maldonis <tomasis7@gmail.com>
2025-04-25 19:50:45 +0200

    Add routing and layout components with error handling and footer links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import PostsPage from "./pages/PostsPage";
+import PostDetailPage from "./pages/PostDetailPage";
+import CreatePostPage from "./pages/CreatePostPage";
+import ProfilePage from "./pages/ProfilePage";
+import AdminPage from "./pages/AdminPage";
+import ErrorPage from "./pages/ErrorPage";
 
 import "./App.css";
 
@@ -8,10 +18,15 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path: "home",
-        element: <Home />,
-      },
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+      { path: "posts", element: <PostsPage /> },
+      { path: "posts/:id", element: <PostDetailPage /> },
+      { path: "create-post", element: <CreatePostPage /> },
+      { path: "profile", element: <ProfilePage /> },
+      { path: "admin", element: <AdminPage /> },
+      { path: "*", element: <ErrorPage /> },
     ],
   },
 ]);
