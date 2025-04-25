8b92f6b36651be2014313aa942599a495b8768b5
Tomas Maldonis <tomasis7@gmail.com>
2025-04-25 20:13:08 +0200

    Add react-router-dom for routing and update layout components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
