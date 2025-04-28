d01c7d0184d0f51c7bcc6daa49fd02cb3f841ed8
Kind606 <kevin.hellgren@hotmail.com>
2025-04-28 10:35:46 +0200

    small fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,7 @@ app.use(
     name: "session",
     keys: [process.env.COOKIE_SECRET ?? "default_secret"], 
     maxAge: 24 * 60 * 60 * 1000, 
-);
+}));
 app.use(postRouter);
 app.use(userRouter);
 app.use(errorHandler);
