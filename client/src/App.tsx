0116ea8a0a1d008b2af978f8f3c3918eec7ba78d
Kind606 <kevin.hellgren@hotmail.com>
2025-04-29 18:40:54 +0200

    added routing for login and register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
-// import LoginPage from "./pages/LoginPage";
-// import RegisterPage from "./pages/RegisterPage";
 import PostsPage from "./pages/PostsPage";
 // import PostDetailPage from "./pages/PostDetailPage";
 // import CreatePostPage from "./pages/CreatePostPage";
@@ -10,6 +8,8 @@ import PostsPage from "./pages/PostsPage";
 import ErrorPage from "./pages/ErrorPage";
 
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const router = createBrowserRouter([
   {
@@ -18,8 +18,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      // { path: "login", element: <LoginPage /> },
-      // { path: "register", element: <RegisterPage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
       { path: "posts", element: <PostsPage /> },
       // { path: "posts/:id", element: <PostDetailPage /> },
       // { path: "create-post", element: <CreatePostPage /> },
