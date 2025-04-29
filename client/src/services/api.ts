9b14f21bfc5b78498e031044810c9d141b87f4dc
Tomas Maldonis <tomasis7@gmail.com>
2025-04-29 18:11:37 +0200

    Fix formatting in api.ts for consistency in comments

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // Important for sending cookies with requests
+  withCredentials: true, // Important  for sending cookies with requests
 });
 
 export default api;
